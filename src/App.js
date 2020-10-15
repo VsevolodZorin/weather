ef73de4e01891f5937f8fa8f07ed44ec9cc65e3a
Maksim Ivanov <satansdeer@gmail.com>
2020-10-15 20:28:02 +0200

    Fetch data from weather api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+const LAT = '33.441792'
+const LON = '94.037689'
+const KEY = process.env.REACT_APP_WEATHER_KEY;
+
+console.log(KEY)
 
 function App() {
+  const [temp , setTemp] = useState(0)
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${LAT}&lon=${LON}&appid=${KEY}`
+      const data = await fetch(url).then(res => res.json())
+
+      const dailyTemperatures = data.daily.map(d => d.temp)
+      const dailyAverages = dailyTemperatures.map(d => (d.min + d.max)/2)
+      const temperatureSum = dailyAverages.reduce((prev, acc) => acc + prev, 0)
+      const averageTemperature = temperatureSum / dailyAverages.length;
+
+      setTemp(averageTemperature)
+    }
+    fetchWeather()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Средняя температура {temp}
+    </>
   );
 }
 
